d601399ed655d19c7fbb0d2ee630ee36bb01e261
Udayan Maurya <edmauryaishu@gmail.com>
2021-06-20 16:35:03 -0400

    Added bottom images

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,47 @@
 import Head from "next/head";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [winWidth, setWinWidth] = useState();
+  useEffect(() => {
+    const handleResize = () => setWinWidth(window.innerWidth);
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  });
   return (
     <div className="min-h-screen">
       <Head>
         <title>Sunnyside Agency</title>
         <link rel="icon" href="/favicon-32x32.png" />
       </Head>
+      <article className="flex flex-wrap">
+        <img
+          src={`/${
+            winWidth >= 648 ? "desktop" : "mobile"
+          }/image-gallery-milkbottles.jpg`}
+          className="w-1/2 sm:w-1/4"
+        ></img>
+        <img
+          src={`/${
+            winWidth >= 648 ? "desktop" : "mobile"
+          }/image-gallery-orange.jpg`}
+          className="w-1/2 sm:w-1/4"
+        ></img>
+        <img
+          src={`/${
+            winWidth >= 648 ? "desktop" : "mobile"
+          }/image-gallery-cone.jpg`}
+          className="w-1/2 sm:w-1/4"
+        ></img>
+        <img
+          src={`/${
+            winWidth >= 648 ? "desktop" : "mobile"
+          }/image-gallery-sugar-cubes.jpg`}
+          className="w-1/2 sm:w-1/4"
+        ></img>
+      </article>
 
       <footer className="py-10 bg-footerCyan text-footerTxt flex justify-center">
         <div className="max-w-prose">
